c6dd2ea1686599f425412fe6c3ffaf994cd2d975
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-09-12 14:54:57 -0400

    Done

--- index.ts
+++ index.ts
@@ -1,18 +1,60 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 import 'dotenv/config';
-import { runGraphInFile } from '@ironclad/rivet-node';
+import ora from 'ora';
+import { globalRivetNodeRegistry, plugins, runGraphInFile } from '@ironclad/rivet-node';
+
+function getAssemblyAiApiKey(){
+  const apiKey = process.env.ASSEMBLYAI_API_KEY;
+  if(!apiKey){
+    throw new Error('ASSEMBLYAI_API_KEY environment variable is not set');
+  }
+  return apiKey;
+}
 
 (async () => {
-  const results = await runGraphInFile('/Users/niels/Downloads/Podcast Q&A.rivet-project', {
+  const rl = readline.createInterface({ input, output });
+  const spinner = ora({discardStdin: false});
+
+  globalRivetNodeRegistry.registerPlugin(plugins.assemblyAi);
+  const pluginSettings = {
+    'assemblyAi': {
+      'assemblyAiApiKey': getAssemblyAiApiKey(),
+    }
+  };
+
+  const audioUrl = await rl.question('Provide the URL of your podcast file: ');
+
+  spinner.start('Transcribing');
+  const createTranscriptResults = await runGraphInFile('./podcast-qna.rivet-project', {
     graph: 'Create transcript',
     inputs: {
-      audio_url: 'https://storage.googleapis.com/aai-docs-samples/podcast.mov',
+      audio_url: audioUrl,
     },
     openAiKey: null as unknown as string,
-    pluginSettings: {
-      'assemblyAi': {
-        'assemblyAiApiKey': process.env.ASSEMBLY_AI_API_KEY,
-      }
-    }
+    pluginSettings
   });
-  console.log(results);
-})(); 
+  const transcriptId = createTranscriptResults.transcript_id.value as string;
+  spinner.succeed("Transcribed");
+
+  while(true){
+    console.log();
+    const answer = await rl.question('Ask a question: ');
+
+    spinner.start("Generating answer");
+    const askQuestionResults = await runGraphInFile('/Users/niels/Downloads/Podcast Q&A.rivet-project', {
+      graph: 'Ask question',
+      inputs: {
+        transcript_id: transcriptId,
+        question: answer
+      },
+      openAiKey: null as unknown as string,
+      pluginSettings
+    });
+    spinner.stop();
+  
+    console.log('Answer:', askQuestionResults.answer.value);
+  }
+
+  rl.close();
+})();
