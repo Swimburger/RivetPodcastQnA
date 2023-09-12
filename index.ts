80d8ace7987b8e42438e40cc97584145f5b80931
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-09-12 17:28:24 -0400

    Refactor to load project, then call graphs

--- index.ts
+++ index.ts
@@ -2,11 +2,18 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 import 'dotenv/config';
 import ora from 'ora';
-import { globalRivetNodeRegistry, plugins, runGraphInFile } from '@ironclad/rivet-node';
+import { globalRivetNodeRegistry, plugins, loadProjectFromFile, runGraph } from '@ironclad/rivet-node';
 
-function getAssemblyAiApiKey(){
+const rivetProjectFile = './podcast-qna.rivet-project';
+const aaiPluginSettings = {
+  'assemblyAi': {
+    'assemblyAiApiKey': getAssemblyAiApiKey(),
+  }
+};
+
+function getAssemblyAiApiKey() {
   const apiKey = process.env.ASSEMBLYAI_API_KEY;
-  if(!apiKey){
+  if (!apiKey) {
     throw new Error('ASSEMBLYAI_API_KEY environment variable is not set');
   }
   return apiKey;
@@ -14,45 +21,43 @@ function getAssemblyAiApiKey(){
 
 (async () => {
   const rl = readline.createInterface({ input, output });
-  const spinner = ora({discardStdin: false});
-
+  const spinner = ora({ discardStdin: false });
   globalRivetNodeRegistry.registerPlugin(plugins.assemblyAi);
-  const pluginSettings = {
-    'assemblyAi': {
-      'assemblyAiApiKey': getAssemblyAiApiKey(),
-    }
-  };
 
+  const project = await loadProjectFromFile(rivetProjectFile);
   const audioUrl = await rl.question('Provide the URL of your podcast file: ');
 
   spinner.start('Transcribing');
-  const createTranscriptResults = await runGraphInFile('./podcast-qna.rivet-project', {
+  const createTranscriptResults = await runGraph(project, {
     graph: 'Create transcript',
     inputs: {
       audio_url: audioUrl,
     },
     openAiKey: null as unknown as string,
-    pluginSettings
+    pluginSettings: aaiPluginSettings
   });
-  const transcriptId = createTranscriptResults.transcript_id.value as string;
   spinner.succeed("Transcribed");
 
-  while(true){
+  // console.debug(createTranscriptResults);
+  const transcriptId = createTranscriptResults.transcript_id.value as string;
+
+  while (true) {
     console.log();
     const answer = await rl.question('Ask a question: ');
 
     spinner.start("Generating answer");
-    const askQuestionResults = await runGraphInFile('/Users/niels/Downloads/Podcast Q&A.rivet-project', {
+    const askQuestionResults = await runGraph(project, {
       graph: 'Ask question',
       inputs: {
         transcript_id: transcriptId,
         question: answer
       },
       openAiKey: null as unknown as string,
-      pluginSettings
+      pluginSettings: aaiPluginSettings
     });
     spinner.stop();
-  
+
+    // console.debug(askQuestionResults);
     console.log('Answer:', askQuestionResults.answer.value);
   }
 
